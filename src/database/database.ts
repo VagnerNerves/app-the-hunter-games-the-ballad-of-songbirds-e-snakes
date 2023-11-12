8909d7dab43f07424b6b4a2e5840bbb21a1d4338
Vagner Nerves <vagnernervessantos@gmail.com>
2023-11-12 14:49:45 -0300

    feat: Created database with trailers.

---
+++ src/database/database.ts
@@ -0,0 +1,14 @@
+export const database = {
+  trailers: [
+    {
+      id: 'trailer1',
+      pathImage: require('../assets/images/trailers/thumb-trailer1.png'),
+      urlVideo: 'https://youtu.be/mIZ4fX4ACUY?si=GQ-phrtBn_HQf1e0'
+    },
+    {
+      id: 'trailer2',
+      pathImage: require('../assets/images/trailers/thumb-trailer2.png'),
+      urlVideo: 'https://youtu.be/psF5B6jJBV4?si=xtzfA5BqdW-nlSTT'
+    }
+  ]
+}
