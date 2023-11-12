41ca9d110cba0058b8b1c5deef1ee9ac2e474b8d
Vagner Nerves <vagnernervessantos@gmail.com>
2023-11-12 09:03:37 -0300

    feat: Add Fonts Roboto and BankGothic.

--- App.tsx
+++ App.tsx
@@ -1,7 +1,19 @@
 import { StatusBar } from 'react-native'
+
+import { useFonts } from 'expo-font'
+import { Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto'
+
+import { Loading } from '@components/Loading'
 import { Routes } from '@routes/index'
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    BankGothic_Light: require('./assets/fonts/BankGothicLight.otf'),
+    BankGothic_Medium: require('./assets/fonts/BankGothicMedium.otf'),
+    Roboto_400Regular,
+    Roboto_700Bold
+  })
+
   return (
     <>
       <StatusBar
@@ -9,7 +21,7 @@ export default function App() {
         backgroundColor="transparent"
         translucent
       />
-      <Routes />
+      {fontsLoaded ? <Routes /> : <Loading />}
     </>
   )
 }
