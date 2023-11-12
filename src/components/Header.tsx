d74ec49016bac26f9111acdcf63c88ef3e367409
Vagner Nerves <vagnernervessantos@gmail.com>
2023-11-12 13:48:54 -0300

    feat: Create component Header.

---
+++ src/components/Header.tsx
@@ -0,0 +1,45 @@
+import {
+  View,
+  Image,
+  TouchableOpacity,
+  Text,
+  ImageSourcePropType,
+  ScrollView
+} from 'react-native'
+import { useSafeAreaInsets } from 'react-native-safe-area-context'
+
+import { useNavigation } from '@react-navigation/native'
+
+import { ArrowCircleLeft } from 'phosphor-react-native'
+
+interface HeaderProps {
+  image: ImageSourcePropType
+  isButtonGoBack?: boolean
+}
+
+export function Header({ image, isButtonGoBack = false }: HeaderProps) {
+  const navigation = useNavigation()
+
+  const insets = useSafeAreaInsets()
+  const topButtonGoBack = insets.top + 4
+
+  function handleGoBack() {
+    navigation.goBack()
+  }
+
+  return (
+    <View className="w-full h-[345px]">
+      {isButtonGoBack && (
+        <TouchableOpacity
+          className="absolute left-6 z-10"
+          style={{ top: topButtonGoBack }}
+          onPress={handleGoBack}
+        >
+          <ArrowCircleLeft weight="fill" size={32} color="#FBF9EB" />
+        </TouchableOpacity>
+      )}
+
+      <Image source={image} className="w-full h-full object-cover" />
+    </View>
+  )
+}
