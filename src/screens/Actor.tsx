9e3ee2d292444b036691241b295d6a8e8aac94f2
Vagner Nerves <vagnernervessantos@gmail.com>
2023-11-11 21:56:26 -0300

    feat: Created Screens.

---
+++ src/screens/Actor.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from 'react-native'
+
+export function Actor() {
+  return (
+    <View className="flex-1 bg-black items-center justify-center">
+      <Text className="text-white"> Screen Actor</Text>
+    </View>
+  )
+}
---
+++ src/screens/Synopsis.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from 'react-native'
+
+export function Synopsis() {
+  return (
+    <View className="flex-1 bg-black items-center justify-center">
+      <Text className="text-white">Screen Synopses</Text>
+    </View>
+  )
+}
