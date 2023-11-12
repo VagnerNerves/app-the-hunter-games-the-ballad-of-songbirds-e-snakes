9bb90532529521edcda4f5ced615700e4f3c9474
Vagner Nerves <vagnernervessantos@gmail.com>
2023-11-11 22:26:14 -0300

    feat: Ajuted background color.

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,12 +1,15 @@
 import { View } from 'react-native'
-import { NavigationContainer } from '@react-navigation/native'
+import { NavigationContainer, DefaultTheme } from '@react-navigation/native'
 
 import { AppRoutes } from './app.routes'
 
 export function Routes() {
+  const theme = DefaultTheme
+  theme.colors.background = '#251309'
+
   return (
-    <View className="flex-1">
-      <NavigationContainer>
+    <View className="flex-1 bg-woodbark-950">
+      <NavigationContainer theme={theme}>
         <AppRoutes />
       </NavigationContainer>
     </View>
--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -2,7 +2,7 @@ import { Text, View } from 'react-native'
 
 export function Home() {
   return (
-    <View className="flex-1 bg-black items-center justify-center">
+    <View className="flex-1 items-center justify-center">
       <Text className="text-white">Screen Home</Text>
     </View>
   )
