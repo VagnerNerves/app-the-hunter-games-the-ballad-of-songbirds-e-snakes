186e88761c10de01e35eb962977cf9b19f6dd767
Vagner Nerves <vagnernervessantos@gmail.com>
2023-11-12 15:15:02 -0300

    feat: Add synopsis

--- src/database/database.ts
+++ src/database/database.ts
@@ -10,5 +10,10 @@ export const database = {
       pathImage: require('../assets/images/trailers/thumb-trailer2.png'),
       urlVideo: 'https://youtu.be/psF5B6jJBV4?si=xtzfA5BqdW-nlSTT'
     }
+  ],
+  synopsis: [
+    'Anos antes de se tornar o tirano Presidente de Panem, Coriolanus Snow, com 18 anos, é a última esperança para sua linhagem enfraquecida, uma família outrora orgulhosa que caiu em desgraça em um Capitol pós-guerra.',
+    'Com os 10º Jogos Vorazes anuais se aproximando rapidamente, o jovem Snow fica alarmado ao ser designado para mentorar Lucy Gray Baird, a tributo feminina do empobrecido Distrito 12. No entanto, depois que Lucy Gray chama a atenção de todo Panem ao cantar desafiadoramente durante a cerimônia de colheita, Snow pensa que pode virar as probabilidades a seu favor.',
+    'Unindo seus instintos para o showbiz e sua astúcia política recém-descoberta, a corrida contra o tempo de Snow e Lucy para sobreviver revelará, em última instância, quem é um pássaro cantor e quem é uma cobra.'
   ]
 }
