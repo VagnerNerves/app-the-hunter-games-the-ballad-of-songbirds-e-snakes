362dd3b18617969b77d24c6ccac590b6693635d4
Vagner Nerves <vagnernervessantos@gmail.com>
2023-11-11 22:23:39 -0300

    feat: Add Colors woodbark.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,24 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./App.tsx", "./src/**/*.{ts,tsx}"],
+  content: ['./App.tsx', './src/**/*.{ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        woodbark: {
+          50: '#FBF9EB',
+          100: '#F6EFCB',
+          200: '#EEDE9A',
+          300: '#E4C560',
+          400: '#DBAD34',
+          500: '#CC9626',
+          600: '#AF761F',
+          700: '#8C551C',
+          800: '#75451E',
+          900: '#643B1F',
+          950: '#251309'
+        }
+      }
+    }
   },
-  plugins: [],
-};
+  plugins: []
+}
